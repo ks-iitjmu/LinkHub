77b0b2d67d184cfcb655b79e16772daa46244d12
Kunal Sharma <159030119+ks-iitjmu@users.noreply.github.com>
2026-03-28 00:24:04 +0530

    add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image"
+
+export default function Footer() {
+  return (
+    <div className="py-10 flex justify-center bg-gray-100">
+        <h1 className="text-gray-600 uppercase text-xs font-medium tracking-widest">&copy; 2026 Kunal Sharma</h1>
+    </div>
+  )
+}
